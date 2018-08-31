6df62eb9216ec2aea6cc27e2ab10664d6dd41815
夜云 <644362887@qq.com>
2018-08-31 16:39:33 +0800

    logo

--- server/server.go
+++ server/server.go
@@ -1,10 +1,24 @@
 package server
 
+import (
+	"fmt"
+	"os"
+	"bnwUdp/share"
+)
+
 func StartServer()  {
 	// 启动 udp 服务
 	go startUdpServer()
 	// 启动 http 服务
 	go startHttp()
+
+	fmt.Printf("%c[1;40;32m%s%c[0m\n\n", 0x1B, "北京帮你玩科技有限公司 UDP 设备换绑记录 服务启动成功", 0x1B)
+	fmt.Println("进程 ID:",  os.Getpid())
+	fmt.Println("UDP Port:",  share.ShareConfig.UdpPort)
+	if share.ShareConfig.HttpPort != "" {
+		fmt.Println("HTTP Port:",  share.ShareConfig.HttpPort)
+		fmt.Println("查看UDP处理情况, 请访问 IP:"+share.ShareConfig.HttpPort)
+	}
 
 	select {
 
--- test/udpClient.go
+++ test/udpClient.go
@@ -18,22 +18,10 @@ func main() {
 	defer socket.Close()
 
 	var json = `{"old_device":"123","new_device":"321","time":555,"secret_key":"8c3c1e"}`
-	//senddata := []byte("hi server!")
 	senddata := []byte(json)
 	_, err = socket.Write(senddata)
 	if err != nil {
 		fmt.Println("send fail !", err)
 		return
 	}
-
-	/*
-	data := make([]byte, 10)
-	read, remoteAddr, err := socket.ReadFromUDP(data)
-	if err != nil {
-		fmt.Println("read fail !", err)
-		return
-	}
-	fmt.Println(read, remoteAddr)
-	fmt.Printf("%s\n", data)
-	*/
 }
